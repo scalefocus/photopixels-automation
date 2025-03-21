6e64a5826ab50e9ca56a0545a0ae6fe80ee4e337
Petya Petarcheva <petya.petarcheva@scalefocus.com>
2025-03-21 18:08:13 +0200

    Adding tests for get changes endpoint and testng suites for api tests

---
+++ src/test/java/com/photopixels/api/sync/GetChangesTests.java
@@ -0,0 +1,60 @@
+package com.photopixels.api.sync;
+
+import com.photopixels.api.dtos.sync.GetChangesResponseDto;
+import com.photopixels.api.steps.sync.GetChangesSteps;
+import com.photopixels.base.ApiBaseTest;
+import com.photopixels.helpers.listeners.StatusTestListener;
+import io.qameta.allure.*;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+@Listeners(StatusTestListener.class)
+@Feature("Sync")
+public class GetChangesTests extends ApiBaseTest {
+
+    private String token;
+
+    @BeforeClass(alwaysRun = true)
+    public void setup() {
+        token = getUserToken();
+    }
+
+    @Test(description = "Get changes")
+    @Description("Get changes")
+    @Story("Get Changes")
+    @Severity(SeverityLevel.NORMAL)
+    public void getChangesTest() {
+        String revisionId = "0";
+
+        GetChangesSteps getChangesSteps = new GetChangesSteps(token);
+        GetChangesResponseDto getChangesResponseDto = getChangesSteps.getChanges(revisionId);
+
+        SoftAssert softAssert = new SoftAssert();
+
+        softAssert.assertFalse(getChangesResponseDto.getAdded().isEmpty(), "Added changes are returned");
+        softAssert.assertTrue(getChangesResponseDto.getDeleted().isEmpty(), "Deleted changes are returned");
+
+        softAssert.assertAll();
+    }
+
+    @Test(description = "Get changes not existing revision id")
+    @Description("Get changes with not existing  revision id")
+    @Story("Get Changes")
+    @Severity(SeverityLevel.MINOR)
+    public void getChangesNotExistingRevisionIdTest() {
+        String revisionId = RandomStringUtils.randomNumeric(6);
+
+        GetChangesSteps getChangesSteps = new GetChangesSteps(token);
+        GetChangesResponseDto getChangesResponseDto = getChangesSteps.getChanges(revisionId);
+
+        SoftAssert softAssert = new SoftAssert();
+
+        softAssert.assertTrue(getChangesResponseDto.getAdded().isEmpty(), "Added changes are returned");
+        softAssert.assertTrue(getChangesResponseDto.getDeleted().isEmpty(), "Deleted changes are returned");
+
+        softAssert.assertAll();
+    }
+}
