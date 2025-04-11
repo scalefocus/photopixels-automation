ae4a9f7cf5f9b1e27c3e1b7af14c1a975a7ea057
Petya Petarcheva <petya.petarcheva@scalefocus.com>
2025-04-11 18:27:31 +0300

    Adding tus suite to all api tests suite

--- src/test/java/com/photopixels/api/tus/PostCreateUploadTests.java
+++ src/test/java/com/photopixels/api/tus/PostCreateUploadTests.java
@@ -16,25 +16,24 @@ import static com.photopixels.constants.Constants.VALID_UPLOAD_METADATA;
 @Listeners(StatusTestListener.class)
 @Feature("Tus")
 public class PostCreateUploadTests extends ApiBaseTest {
+
     private String token;
-    private PostCreateUploadSteps steps;
 
     @BeforeClass(alwaysRun = true)
     public void setup() {
         token = getAdminToken();
-        steps = new PostCreateUploadSteps(token);
     }
 
     @Test(description = "Successfully create upload")
     @Description("Positive test: Verify that an upload resource is created successfully")
     @Story("Create Upload")
     @Severity(SeverityLevel.NORMAL)
-
     public void createUploadSuccessfully() {
 
         //TODO:Replace hardcoded uploadMetadata and uploadLength with dynamically generated values (e.g. encode fileName, extension, etc.)
 
-        Response response = steps.createUpload(VALID_UPLOAD_METADATA, VALID_UPLOAD_LENGTH);
+        PostCreateUploadSteps postCreateUploadSteps = new PostCreateUploadSteps(token);
+        Response response = postCreateUploadSteps.createUpload(VALID_UPLOAD_METADATA, VALID_UPLOAD_LENGTH);
 
         SoftAssert softAssert = new SoftAssert();
 
@@ -50,7 +49,8 @@ public class PostCreateUploadTests extends ApiBaseTest {
     public void createUploadWithoutMetadata() {
         String uploadMetadata = "";
 
-        String responseBody = steps.createUploadError(uploadMetadata, VALID_UPLOAD_LENGTH);
+        PostCreateUploadSteps postCreateUploadSteps = new PostCreateUploadSteps(token);
+        String responseBody = postCreateUploadSteps.createUploadError(uploadMetadata, VALID_UPLOAD_LENGTH);
 
         SoftAssert softAssert = new SoftAssert();
 
@@ -66,7 +66,8 @@ public class PostCreateUploadTests extends ApiBaseTest {
     public void createUploadWithoutLength() {
         String uploadLength = "";
 
-        String responseBody = steps.createUploadError(VALID_UPLOAD_METADATA, uploadLength);
+        PostCreateUploadSteps postCreateUploadSteps = new PostCreateUploadSteps(token);
+        String responseBody = postCreateUploadSteps.createUploadError(VALID_UPLOAD_METADATA, uploadLength);
 
         SoftAssert softAssert = new SoftAssert();
 
