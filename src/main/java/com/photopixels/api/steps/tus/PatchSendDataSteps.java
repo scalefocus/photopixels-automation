40596b77ed807d1d3fc4ac99d62d081c77e5e03e
Petya Petarcheva <petya.petarcheva@scalefocus.com>
2025-05-13 10:49:00 +0300

    Fixing request content type

--- src/main/java/com/photopixels/api/steps/tus/PatchSendDataSteps.java
+++ src/main/java/com/photopixels/api/steps/tus/PatchSendDataSteps.java
@@ -3,13 +3,14 @@ package com.photopixels.api.steps.tus;
 import com.photopixels.helpers.CustomRequestSpecification;
 import com.photopixels.helpers.RequestOperationsHelper;
 import io.qameta.allure.Step;
-
 import io.restassured.response.Response;
 
 import java.io.File;
 import java.util.Map;
 
 import static com.photopixels.constants.BasePathsConstants.PATCH_SEND_DATA;
+import static io.restassured.RestAssured.config;
+import static io.restassured.config.EncoderConfig.encoderConfig;
 import static org.apache.hc.core5.http.HttpStatus.SC_NO_CONTENT;
 
 public class PatchSendDataSteps {
@@ -37,11 +38,13 @@ public class PatchSendDataSteps {
         // Add the location ID to the request path
         requestSpecification.addPathParams(Map.of("locationId", locationId)); // Ensure this is set
 
+        config = config().encoderConfig(encoderConfig()
+                .appendDefaultContentCharsetToContentTypeIfUndefined(false));
+
         // Add the required headers for the request
         requestSpecification.addCustomHeader("Tus-Resumable", "1.0.0");
         requestSpecification.addCustomHeader("Upload-Offset", String.valueOf(uploadOffset));
         requestSpecification.addCustomHeader("Upload-Metadata", uploadMetadata);
-        requestSpecification.addCustomHeader("Content-Length", String.valueOf(contentLength));
         requestSpecification.addCustomHeader("Content-Type", "application/offset+octet-stream");
 
         // Send the PATCH request and return the response
