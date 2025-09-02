a4d0ecfd104d84d5c34c7ade9605fe53d499d3a9
andon.staykov <astaykov@eservicios.indracompany.com>
2025-09-02 17:58:56 +0300

    PART4 - add parametrized negative test for empty required fields

--- src/main/java/com/photopixels/mobile/pages/RegistrationPage.java
+++ src/main/java/com/photopixels/mobile/pages/RegistrationPage.java
@@ -26,7 +26,7 @@ public class RegistrationPage extends WaitOperationHelper {
     @AndroidFindBy(xpath = "//android.widget.TextView[@text='Confirm password']/parent::android.widget.EditText")
     private WebElement confirmPasswordField;
 
-    @AndroidFindBy(xpath = "//android.widget.TextView[@text='REGISTER']")
+    @AndroidFindBy(xpath = "//android.widget.TextView[@text='REGISTER']/following-sibling::android.widget.Button")
     private WebElement registerButton;
 
     @AndroidFindBy(xpath = "//android.widget.TextView[@text=\"Name\"]/following-sibling::android.widget.TextView")
@@ -41,39 +41,43 @@ public class RegistrationPage extends WaitOperationHelper {
     }
 
     @Step("Successful registration of a new user")
-    public MobileLoginPage registerNewUser(String name, String email, String password) {
-        fillNameField(name);
-        fillEmailField(email);
-        fillPasswordField(password);
-        fillConfirmPasswordField(password);
+    public MobileLoginPage registerNewUser(String name, String email, String password, String confirmPassword) {
+        fillInRegistrationFields(name, email, password, confirmPassword);
         registerButton.click();
         return new MobileLoginPage(driver);
     }
 
+    @Step("Fill in all registration fields")
+    public void fillInRegistrationFields(String name, String email, String password, String confirmPassword){
+        fillNameField(name);
+        fillEmailField(email);
+        fillPasswordField(password);
+        fillConfirmPasswordField(confirmPassword);
+    }
     @Step("Fill in Name field")
     public void fillNameField(String name) {
-        if (name != null) {
+        if (name != null && !name.isEmpty()) {
             nameField.sendKeys(name);
         }
     }
 
     @Step("Fill in Email field")
     public void fillEmailField(String email) {
-        if (email != null) {
+        if (email != null && !email.isEmpty()) {
             emailField.sendKeys(email);
         }
     }
 
     @Step("Fill in Password field")
     public void fillPasswordField(String password) {
-        if (password != null) {
+        if (password != null && !password.isEmpty()) {
             passwordField.sendKeys(password);
         }
     }
 
     @Step("Fill in Confirm Password field")
     public void fillConfirmPasswordField(String confirmPassword) {
-        if (confirmPassword != null) {
+        if (confirmPassword != null && !confirmPassword.isEmpty()) {
             confirmPasswordField.sendKeys(confirmPassword);
         }
     }
@@ -86,6 +90,11 @@ public class RegistrationPage extends WaitOperationHelper {
         } catch (Exception e) {
             return false;
         }
+    }
+
+    @Step("Check if Register button is enabled")
+    public boolean isRegisterButtonEnabled() {
+        return "true".equals(registerButton.getAttribute("clickable"));
     }
 
 }
